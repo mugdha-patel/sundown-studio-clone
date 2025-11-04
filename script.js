b188cbf2e3b8ad4c8a096924b161b325b03ae22e
Mugdha <mugdha220598@gmail.com>
2025-11-04 22:45:54 +0000

    page 4 with animation

--- script.js
+++ script.js
@@ -5,17 +5,38 @@ const scroll = new LocomotiveScroll({
 
 var elemC = document.querySelector(".elem-container")
 var fixed = document.querySelector(".fixed-image")
-elemC.addEventListener("mouseenter", function(){
+elemC.addEventListener("mouseenter", function () {
     fixed.style.display = "block"
 })
-elemC.addEventListener("mouseleave", function(){
+elemC.addEventListener("mouseleave", function () {
     fixed.style.display = "none"
 })
 
 var elems = document.querySelectorAll(".elem")
-elems.forEach(function(e){
-    e.addEventListener("mouseenter", function(){
+elems.forEach(function (e) {
+    e.addEventListener("mouseenter", function () {
         var image = e.getAttribute("data-image")
         fixed.style.backgroundImage = `url(${image})`
     })
 })
+
+
+function swiperAnimation() {
+    var swiper = new Swiper(".mySwiper", {
+        slidesPerView: "auto",
+        spaceBetween: 100,
+        slidesPerView: "auto",
+        grabCursor: true,
+        speed: 3000,
+        spaceBetween: 30,
+        freeMode: true,
+        loop: true,
+        centeredSlides: true,
+        autoplay: {
+          delay: 0,
+          disableOnInteraction: false
+        }
+    });
+}
+
+swiperAnimation()
